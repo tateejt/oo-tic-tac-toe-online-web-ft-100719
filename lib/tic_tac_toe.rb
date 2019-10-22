69c15d39875578e922e6c4a9c37fe4da7f787d11
Emery Tate <tateejt@gmail.com>
2019-10-22 01:28:18 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -80,6 +80,6 @@ def won?
   WIN_COMBINATIONS.find{|win_combo| (@board[win_combo[0]] == @board[win_combo[1]]) && (@board[win_combo[2]] == @board[win_combo[0]]) && @board[win_combo[0]] != " "}
 end
 def full? 
-  if @board.include?
+  if @board.include?(" ") || @board.include("")
 
 end
